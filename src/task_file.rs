c47d8901202a11a8ed4fd173bad69b22dd5e64bd
ales <0rbit3r@seznam.cz>
2026-08-05 14:06:11 +0200

    space-prefix title fix and better test file

--- src/task_file.rs
+++ src/task_file.rs
@@ -8,6 +8,7 @@ pub struct TaskFile {
 }
 
 impl TaskFile {
+
     pub fn from_file(file_path: &str) -> Option<TaskFile> {
         let content = fs::read_to_string(file_path).ok()?;
         let mut file = TaskFile::from_string(&content)?;
@@ -15,6 +16,7 @@ impl TaskFile {
 
         Some(file)
     }
+
     pub fn from_string(content: &str) -> Option<TaskFile> {
         //Use result instead
 
@@ -43,13 +45,13 @@ impl TaskFile {
     }
 }
 fn parse_line_and_add_to_task_list(line: &str, tasks: &mut Vec<Task>) -> Option<()> {
-    let mut add_task = |title: &str, state: TaskState| {
+    fn add_task(tasks: &mut Vec<Task>, title: &str, state: TaskState) {
         tasks.push(Task {
             title: title.to_string(),
             state,
             subtasks: vec![],
         })
-    };
+    }
 
     let valid_states = [
         TaskState::Untouched,
@@ -59,24 +61,26 @@ fn parse_line_and_add_to_task_list(line: &str, tasks: &mut Vec<Task>) -> Option<
     ];
 
     let matched = valid_states.iter().find_map(|vs| {
-        let rest_of_line = line.strip_prefix(vs.decoration())?;
+        let rest_of_line = line.strip_prefix(vs.decoration())?.strip_prefix(" ")?;
         Some((rest_of_line, *vs))
     });
 
     match matched {
         Some(m) => {
-            add_task(m.0, m.1);
+            add_task(tasks, m.0, m.1);
             return Some(());
         }
         None => {
             let subtask_line = line.strip_prefix("    ");
             match subtask_line {
                 Some(l) => {
-                    let last_task = tasks.last_mut()?;
-                    parse_line_and_add_to_task_list(l, &mut last_task.subtasks)?;
+                    match tasks.last_mut() {
+                        Some(last_task) => parse_line_and_add_to_task_list(l, &mut last_task.subtasks)?,
+                        None => add_task(tasks, line, TaskState::Corrupted)
+                    };
                     return Some(());
                 }
-                None => add_task(line, TaskState::Corrupted),
+                None => add_task(tasks, line, TaskState::Corrupted),
             }
         }
     }
